b22104bb32967c4ccfda5f2e7a9e1d8d4a784246
tchassijordan <tchassijordan@gmail.com>
2024-10-21 18:15:07 +0100

    feat(layout): create app shell layout

---
+++ src/layout/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react';
+import Navbar from './navbar';
+
+interface AppShellLayoutProps {
+  children: ReactNode;
+}
+
+const AppShellLayout = ({ children }: AppShellLayoutProps) => {
+  return (
+    <main>
+      <header>
+        <Navbar navLinks={NAV_LINKS} />
+      </header>
+      <div className="container margin-block-md">{children}</div>
+    </main>
+  );
+};
+
+const NAV_LINKS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+  {
+    label: 'Contact',
+    href: '/contact',
+  },
+];
+
+export default AppShellLayout;
