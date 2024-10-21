0ff85e68cd6acfb703d1c9432f5119ec1439ff02
tchassijordan <tchassijordan@gmail.com>
2024-10-21 21:21:26 +0100

    feat(app): fetch table data dynamically from api

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,16 @@ import AppShellLayout from './layout';
 import DataGrid from './components/data-grid/table';
 import SearchBar from './components/search-bar/search-bar';
 
-// ** Data
-import { mockTableData } from './components/data-grid/constant';
-
 // ** Column Defs
 import tableColumnsDefs from './components/data-grid/table-columns-defs';
 
+// ** Hook Imports
+import useGetData from './lib/hooks/useGetData';
+
 export default function App() {
   const [query, setQuery] = useState('');
+
+  const { data, loading } = useGetData(query);
 
   return (
     <AppShellLayout>
@@ -25,7 +27,7 @@ export default function App() {
           onInputChange={(query) => setQuery(query)}
           onClear={() => setQuery('')}
         />
-        <DataGrid data={mockTableData} columns={tableColumnsDefs} />
+        <DataGrid data={data} columns={tableColumnsDefs} isLoading={loading} />
       </div>
     </AppShellLayout>
   );
