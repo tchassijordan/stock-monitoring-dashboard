6ccd2f6b4ab32d41df880fb106b17a1c297046c4
tchassijordan <tchassijordan@gmail.com>
2024-10-21 18:10:56 +0100

    style: add base SCSS file for global styles and layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './base.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
