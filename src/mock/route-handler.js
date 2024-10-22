2a9e99286dd017c3253bcec3f138a873f77e214f
tchassijordan <tchassijordan@gmail.com>
2024-10-22 10:58:56 +0100

    feat(mock): update mock data api handler to support filters Filters supported include name/symbol, price, changePercentage

--- src/mock/route-handler.js
+++ src/mock/route-handler.js
@@ -5,16 +5,38 @@ const routeHandler = (req, res) => {
 
   // eslint-disable-next-line eqeqeq
   if (Object.keys(reqSearchParams).length == 0) {
-    // Simulate a slow network request
-    return setTimeout(() => {
-      res.json(mockTableData.slice(0, 10));
-    }, 2_000);
+    return res.json(mockTableData.slice(0, 10));
   }
 
-  // Simulate a slow network request
+  const { q, percentage_change, price } = reqSearchParams;
+
+  const filteredData = mockTableData.filter((data) => {
+    const matchName = q && !data.name.toLowerCase().includes(q.toLowerCase());
+    const matchSymbol =
+      q && !data.symbol.toLowerCase().includes(q.toLowerCase());
+    const matchPercentageChange =
+      data.changePercent > Number(percentage_change);
+    const matchPrice = data.price > Number(price);
+
+    if (matchName || matchSymbol) {
+      return false;
+    }
+
+    if (percentage_change && matchPercentageChange) {
+      return false;
+    }
+
+    if (price && matchPrice) {
+      return false;
+    }
+
+    return true;
+  });
+
+  // Simulating a delay of 1 second
   setTimeout(() => {
-    res.json(mockTableData);
-  }, 2_000);
+    res.json(filteredData);
+  }, 1000);
 };
 
 module.exports = { routeHandler };
